51d176283f4af259f7f563842af39eb6065084e4
Alan Jhonnes <alanjhonnes@gmail.com>
2019-08-14 03:11:43 -0300

    merge examples

--- src/15.util-types.ts
+++ src/15.util-types.ts
@@ -13,3 +13,15 @@ export type RequiredUser = Required<User>;
 export type UserId = Pick<RequiredUser, "id">;
 
 export type InsertUser = Omit<User, "id">;
+
+
+
+export function aLotOfArguments(a: string, b: number, c: boolean): boolean {
+    return true;
+}
+
+export type Arguments = Parameters<typeof aLotOfArguments>;  // [string, number, boolean]
+
+const fnArguments: Arguments = ["a", 2, true]; // as const;
+
+aLotOfArguments(...fnArguments);
--- src/18.parameters.ts
+++
@@ -1,9 +0,0 @@
-export function aLotOfArguments(a: string, b: number, c: boolean): boolean {
-    return true;
-}
-
-export type Arguments = Parameters<typeof aLotOfArguments>;  // [string, number, boolean]
-
-const fnArguments: Arguments = ["a", 2, true]; // as const;
-
-aLotOfArguments(...fnArguments);
